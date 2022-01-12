6fdc97e4daad5c328910c0930039ed34b2904374
Frankie Tsoi <email@example.com>
2022-01-12 13:26:22 +1100

    tictactoe update 6

--- js/main.js
+++ js/main.js
@@ -1,62 +1,73 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('tic tac toe')
 
     let playerTurn = true;
-    let player1 = 'x';
-    let player2 = 'o';
-    
-    const winning = function(){ //here is to select individual cell and print x to the winning pattern
-        if ($("#1").html() === "x" && $("#2").html() === "x" && $("#3").html() === "x" || 
-            $("#4").html() === "x" && $("#5").html() === "x" && $("#6").html() === "x" || 
+    let playerX = $('.player-x-score');
+    let playerO = $('.player-o-score');
+    let draw = $('.draw-score');
+    let xScoreUpdated = 0;
+    let oScoreUpdated = 0;
+    let drawScoreUpdate = 0;
+
+
+    const winning = function () { //here is to select individual cell and print x to the winning pattern
+        if ($("#1").html() === "x" && $("#2").html() === "x" && $("#3").html() === "x" ||
+            $("#4").html() === "x" && $("#5").html() === "x" && $("#6").html() === "x" ||
             $("#7").html() === "x" && $("#8").html() === "x" && $("#9").html() === "x" ||
-            $("#1").html() === "x" && $("#4").html() === "x" && $("#7").html() === "x" ||  
-            $("#2").html() === "x" && $("#5").html() === "x" && $("#8").html() === "x" || 
-            $("#3").html() === "x" && $("#6").html() === "x" && $("#9").html() === "x" || 
-            $("#1").html() === "x" && $("#5").html() === "x" && $("#9").html() === "x" || 
-            $("#3").html() === "x" && $("#5").html() === "x" && $("#7").html() === "x" ){
-            alert("The winner is: X");
+            $("#1").html() === "x" && $("#4").html() === "x" && $("#7").html() === "x" ||
+            $("#2").html() === "x" && $("#5").html() === "x" && $("#8").html() === "x" ||
+            $("#3").html() === "x" && $("#6").html() === "x" && $("#9").html() === "x" ||
+            $("#1").html() === "x" && $("#5").html() === "x" && $("#9").html() === "x" ||
+            $("#3").html() === "x" && $("#5").html() === "x" && $("#7").html() === "x") {
+            alert("The winner is: X");// try to popup a div
+            xScoreUpdated = xScoreUpdated + 1;
+            playerX.html(xScoreUpdated);
             // console.log('the winner is X');
 
         } else if ( // here is to select individual cell and print o to the winning pattern
-            $("#1").html() === "o" && $("#2").html() === "o" && $("#3").html() === "o" || 
-            $("#4").html() === "o" && $("#5").html() === "o" && $("#6").html() === "o" || 
+            $("#1").html() === "o" && $("#2").html() === "o" && $("#3").html() === "o" ||
+            $("#4").html() === "o" && $("#5").html() === "o" && $("#6").html() === "o" ||
             $("#7").html() === "o" && $("#8").html() === "o" && $("#9").html() === "o" ||
-            $("#1").html() === "o" && $("#4").html() === "o" && $("#7").html() === "o" ||  
-            $("#2").html() === "o" && $("#5").html() === "o" && $("#8").html() === "o" || 
-            $("#3").html() === "o" && $("#6").html() === "o" && $("#9").html() === "o" || 
-            $("#1").html() === "o" && $("#5").html() === "o" && $("#9").html() === "o" || 
-            $("#3").html() === "o" && $("#5").html() === "o" && $("#7").html() === "o" ){
+            $("#1").html() === "o" && $("#4").html() === "o" && $("#7").html() === "o" ||
+            $("#2").html() === "o" && $("#5").html() === "o" && $("#8").html() === "o" ||
+            $("#3").html() === "o" && $("#6").html() === "o" && $("#9").html() === "o" ||
+            $("#1").html() === "o" && $("#5").html() === "o" && $("#9").html() === "o" ||
+            $("#3").html() === "o" && $("#5").html() === "o" && $("#7").html() === "o") {
             alert("The winner is: O");
+            oScoreUpdated = oScoreUpdated + 1;
+            playerO.html(oScoreUpdated);
             // console.log('the winner is O');
 
-        } else if ($('.cell:empty').length === 0){ // here is if all cell are filled, it will return a draw
+        } else if ($('.cell:empty').length === 0) { // here is if all cell are filled, it will return a draw
             // console.log('It\'s a draw');
             alert('Too bad it\'s a draw! Start again')
+            drawScoreUpdate = drawScoreUpdate + 1;
+            draw.html(drawScoreUpdate);
         }
     }
 
     //on click function for all <div class="cell"> 9 cells 
     //all cells shared the same on click function
-    $('.cell').on('click', function(){ 
+    $('.cell').on('click', function () {
         //because all the cells share the same on click function we be able to tell    
         //which one was click, and jquery will give us the click DOM node in the variable "this"
         // console.log('check on click working on cells', $(this).html());
-       
+
         // here first check if the cell is empty. you do this after coding the play1turn function. 
-        if( $(this).html() === 'x' || $(this).html() === 'o' ){ // check if x is placed
+        if ($(this).html() === 'x' || $(this).html() === 'o') { // check if x is placed
             // console.log('x and o already here');
             return // by returning this it will stop the function 
         } else {
             // console.log('when cell is empty');
         }
 
-        if (playerTurn === true){ // if playerTurn is true then:              
-            
+        if (playerTurn === true) { // if playerTurn is true then:              
+
             $(this).html('x'); // set the html in the clicked element to 'x'
             playerTurn = false; // playerTurn is over now, so set playerTurn to false.
-            
+
             winning()
-        
+
         } else { // if playerTurn is NOT true, then do this:
 
             $(this).html('o'); // set the html in the clicked element to 'o'
@@ -67,7 +78,7 @@ $(document).ready(function(){
 
     }); // end of on click function
 
-    $('#gameRestart').on('click', function(){ // here is for the restart button
+    $('#gameRestart').on('click', function () { // here is for the restart button
         // console.log('check restart button working?'); 
         $('.cell').empty(); // use .empty() function to clear the cells.
     })
