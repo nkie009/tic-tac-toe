cbe957866001535b3078cae5738d04744314933a
Frankie Tsoi <email@example.com>
2022-01-15 17:30:01 +1100

    tictactoe updated Ai

--- js/main-ai.js
+++ js/main-ai.js
@@ -9,10 +9,10 @@ $(document).ready(function () {
     let xScoreUpdated = 0; // X player here is to set the score to update to html
     let oScoreUpdated = 0; //O player here is to set the score to update to html
     let drawScoreUpdate = 0;// Draw here is to set the score to update to html
-    
 
 
-    const winning = function (player) { //here is to select individual cell and print x to the winning pattern
+
+    const winning = function (player) { //here is to select individual cell and print x o to the winning pattern
         if ($("#1").html() === player && $("#2").html() === player && $("#3").html() === player ||
             $("#4").html() === player && $("#5").html() === player && $("#6").html() === player ||
             $("#7").html() === player && $("#8").html() === player && $("#9").html() === player ||
@@ -21,17 +21,17 @@ $(document).ready(function () {
             $("#3").html() === player && $("#6").html() === player && $("#9").html() === player ||
             $("#1").html() === player && $("#5").html() === player && $("#9").html() === player ||
             $("#3").html() === player && $("#5").html() === player && $("#7").html() === player) {
-            
-                if (player === "x"){ // here is to updated player X score using "if else"
-                    xScoreUpdated = xScoreUpdated + 1; // here is to add the winning score by 1 to the current score board
-                    playerX.html(xScoreUpdated);// here is to updated the score in the html
-                    $('.popup1').addClass('show');// here is to let the popup div to show when X is the winner
 
-                } else if (player ==='o'){ // here is to updated the player o score
-                    oScoreUpdated = oScoreUpdated + 1;
-                    playerO.html(oScoreUpdated);
-                    $('.popup2').addClass('show');
-                }// end of player o wins
+            if (player === "x") { // here is to updated player X score using "if else"
+                xScoreUpdated = xScoreUpdated + 1; // here is to add the winning score by 1 to the current score board
+                playerX.html(xScoreUpdated);// here is to updated the score in the html
+                $('.popup1').addClass('show');// here is to let the popup div to show when X is the winner
+
+            } else { // here is to updated the player o score
+                oScoreUpdated = oScoreUpdated + 1;
+                playerO.html(oScoreUpdated);
+                $('.popup2').addClass('show');
+            }// end of player o wins
 
         } else if ($('.cell:empty').length === 0) { // here is if all cell are filled, it will return a draw
             drawScoreUpdate = drawScoreUpdate + 1;
@@ -42,6 +42,18 @@ $(document).ready(function () {
 
     }; // end of winning
 
+    const aiMove = function () { // here is the ai function starts
+        // console.log('ai turn');
+        // 1. find an empty cell
+        //      a. get a list of empty cell
+        const emptyCells = $('.cell:empty');
+        //      b. select one from the array
+        // console.log(emptyCells[1]);
+        // 2. place o onto selected empty cell
+        const randomIndex = Math.floor(Math.random() * emptyCells.length);
+        $(emptyCells[randomIndex]).html('o')
+        winning('o')// call the winning function to kick in the if else on line 30
+    }; // end of aiMove()
 
     //on click function for all <div class="cell"> 9 cells 
     //all cells shared the same on click function
@@ -56,21 +68,18 @@ $(document).ready(function () {
             return // by returning this it will stop the function 
 
         } else {
-            // console.log('when cell is empty');
+            // console.log('w`hen cell is empty');
         }
 
-        if (playerTurn === true) { // if playerTurn is true then:  
+        // human move
+        $(this).html('x'); // set the html in the clicked cell to 'x'
+        playerTurn = false; // when playerTurn has finish, it will set the playerTurn false
+        winning('x')// here to run winning condition to the player turn.
 
-            $(this).html('x'); // set the html in the clicked cell to 'x'
-            playerTurn = false; // when playerTurn has finish, it will set the playerTurn false
-            winning('x')// here to run winning condition to the player turn.
+        // AI move
+        aiMove();
+        playerTurn = true; // switch to human move for next click
 
-        } else { // if playerTurn is NOT true, then do this:
-
-            $(this).html('o'); // set the html in the clicked cell to 'o'
-            playerTurn = true; // its going to be player turn next click.
-            winning('o')
-        }
     }); // end of on click function
 
     $('#gameAgain').on('click', function () { // here is for the restart button 1
